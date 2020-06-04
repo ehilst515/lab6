54edc6795c1302b56cc1594f047cf02f4d1aa3d8
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-04 11:48:56 -0500

    wrapped logic in function

--- js/add-content.js
+++ js/add-content.js
@@ -1,15 +1,20 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+function timedGreeting() {
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-}   else if (hourNow > 12){
-    greeting = 'Good afternoon!';
-}   else if (hourNow > 0) {
-    greeting = 'Good morning!';
-}   else {
-    greeting = 'Welcome!';
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+    
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    }   else if (hourNow > 12){
+        greeting = 'Good afternoon!';
+    }   else if (hourNow > 0) {
+        greeting = 'Good morning!';
+    }   else {
+        greeting = 'Welcome!';
+    }
+    
+    document.write('<h3>' + greeting + '</h3>');
 }
 
-document.write('<h3>' + greeting + '</h3>');
+timedGreeting();
